165785a103ae64cdc569a628fadc6d810e37a146
Robert Watt <rwatt@four51.com>
2020-05-21 14:32:08 -0500

    :construction: Basic ngbCarousel example

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -8,5 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class CarouselComponent implements OnInit {
   constructor() {}
 
+  public images = [944, 1011, 984].map(
+    (n) => `https://picsum.photos/id/${n}/1700/500`
+  );
+
   ngOnInit(): void {}
 }
--- src/storybook/carousel/carousel.stories.ts
+++ src/storybook/carousel/carousel.stories.ts
@@ -1,11 +1,12 @@
 import { storiesOf } from '@storybook/angular';
 import { CarouselComponent } from 'src/app/components/carousel/carousel.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 storiesOf('Carousel', module).add('Basic Example', () => ({
   component: CarouselComponent,
   moduleMetadata: {
     declarations: [],
-    imports: [],
+    imports: [NgbCarouselModule],
     providers: [],
     entryComponents: [],
   },
