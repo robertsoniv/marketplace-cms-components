b960c624f636dd6623746da4f5a0e9f18a03fb67
Robert Watt <rwatt@four51.com>
2020-05-26 15:33:47 -0500

    :construction: basic custom plugin example
    followed along with https://www.tiny.cloud/docs/advanced/creating-a-plugin/ and added the plugin to the global scripts in angular.json

--- src/storybook/html-editor.stories.ts
+++ src/storybook/html-editor.stories.ts
@@ -1,14 +1,6 @@
-import { PageComponent } from '../app/components/page/page.component';
-import { ContentContainerComponent } from '../app/components/content-container/content-container.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Meta, Title } from '@angular/platform-browser';
-import { Renderer2 } from '@angular/core';
-import { ContentBlockComponent } from '../app/components/content-block/content-block.component';
-import { SafeHtmlPipe } from 'src/app/pipes/safe-html.pipe';
 import { moduleMetadata } from '@storybook/angular';
-import { PageContentDoc } from 'src/app/models/PageContentDoc.interface';
-import { HtmlEditorComponent } from 'src/app/components/html-editor/html-editor.component';
 import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
+import { HtmlEditorComponent } from 'src/app/components/html-editor/html-editor.component';
 
 export default {
   title: 'HTML Editor',
