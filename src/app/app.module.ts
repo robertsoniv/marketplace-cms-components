2236bfd7296bb1aea6dcd4e9ae1f2c4ad39a24a6
Robert Watt <rwatt@four51.com>
2020-05-27 17:26:11 -0500

    :wrench: development workflow
    all tinymce plugins are now included in angular.json scripts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { PageComponent } from './components/page/page.component';
-import { ContentContainerComponent } from './components/content-container/content-container.component';
-import { ContentBlockComponent } from 'src/app/components/content-block/content-block.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import { BgColorDirective } from './directives/bg-color.directive';
-import { RowComponent } from './components/row/row.component';
-import { SafeHtmlPipe } from './pipes/safe-html.pipe';
 import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
+import { AppComponent } from './app.component';
 import { HtmlEditorComponent } from './components/html-editor/html-editor.component';
-import { ColumnComponent } from './components/column/column.component';
-import { OrderCloudModule, Configuration } from '@ordercloud/angular-sdk';
-
-import { CookieModule } from 'ngx-cookie';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PageComponent,
-    ContentContainerComponent,
-    ContentBlockComponent,
-    CarouselComponent,
-    BgColorDirective,
-    RowComponent,
-    SafeHtmlPipe,
-    HtmlEditorComponent,
-    ColumnComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule,
-    EditorModule,
-    CookieModule.forRoot(),
-    OrderCloudModule.forRoot(() => new Configuration({}))
-  ],
+  declarations: [AppComponent, HtmlEditorComponent],
+  imports: [BrowserModule, NgbModule, EditorModule],
   providers: [
     { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' }
   ],
--- src/app/components/html-editor/html-editor.component.ts
+++ src/app/components/html-editor/html-editor.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { OcTokenService } from '@ordercloud/angular-sdk';
 
 @Component({
   selector: 'cms-html-editor',
@@ -7,14 +6,11 @@ import { OcTokenService } from '@ordercloud/angular-sdk';
   styleUrls: ['./html-editor.component.scss']
 })
 export class HtmlEditorComponent implements OnInit {
-  @Input() ocCookieName: string;
+  @Input() ocToken: string;
   @Input() remoteCss: string;
+  @Input() initialValue: string;
 
-  constructor(private ocTokenService: OcTokenService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.ocTokenService.SetAccess(
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c3IiOiJhbm9uLXRlbXBsYXRlLXVzZXIiLCJjaWQiOiJlOTU1ZDZlYy01OWM4LTQ0NDItOTQ4ZS0xNmNjYzVmZDI3YWUiLCJvcmRlcmlkIjoibXRXUnYtT1lvVXFhekQyRm5yVWpwdyIsInUiOiIxMDIwMjMwIiwidXNydHlwZSI6ImJ1eWVyIiwicm9sZSI6WyJNZUFkZHJlc3NBZG1pbiIsIk1lQWRtaW4iLCJNZUNyZWRpdENhcmRBZG1pbiIsIk1lWHBBZG1pbiIsIlNob3BwZXIiLCJTdXBwbGllclJlYWRlciIsIlN1cHBsaWVyQWRkcmVzc1JlYWRlciIsIlBhc3N3b3JkUmVzZXQiLCJCdXllclJlYWRlciJdLCJpc3MiOiJodHRwczovL2F1dGgub3JkZXJjbG91ZC5pbyIsImF1ZCI6Imh0dHBzOi8vYXBpLm9yZGVyY2xvdWQuaW8iLCJleHAiOjE1OTEyMDQ0MzYsIm5iZiI6MTU5MDU5OTYzNn0.JvRJnjNaYwSlh-4k1Nk6xevT5qLrCcVjvfH56zME6VQ'
-    );
-  }
+  ngOnInit(): void {}
 }
