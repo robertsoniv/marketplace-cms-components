e00534eccc6f01355963698e5be49a477ed574d3
Crhistian Ramirez <cramirez@four51.com>
2020-06-09 15:17:08 -0500

    cms images
    d

--- src/app/components/html-editor/html-editor.component.ts
+++ src/app/components/html-editor/html-editor.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import cmsUpload from './cms-asset-upload/cms-asset-upload'
+import cmsPicker from './cms-asset-picker/cms-asset-picker';
+import * as MarketplaceSdkInstance from 'marketplace-javascript-sdk';
+import { MarketplaceSDK } from 'marketplace-javascript-sdk';
 
 @Component({
   selector: 'cms-html-editor',
@@ -10,7 +14,46 @@ export class HtmlEditorComponent implements OnInit {
   @Input() remoteCss: string;
   @Input() initialValue: string;
 
-  constructor() {}
+  editorOptions = {
+    base_url: '/tinymce',
+    suffix: '.min',
+    content_css: this.remoteCss,
+    content_style: 'body {padding:15px !important;}',
+    height: 500,
+    menubar: 'insert',
+    plugins: [
+      'image table advlist autolink lists link charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar:
+      'oc-product image undo redo | formatselect | bold italic backcolor | \
+      alignleft aligncenter alignright alignjustify | \
+      bullist numlist outdent indent table | removeformat | help',
+    ocToken: this.ocToken,
 
-  ngOnInit(): void {}
+    /**
+     * Allows user to browse and select images from ordercloud cms
+     */
+    file_picker_callback: cmsPicker,
+
+    /**
+     * Adds an advanced tab to set things like style/border/space
+     */
+    image_advtab: true,
+
+    /**
+     * Adds an upload tab (uploads to ordercloud cms)
+     */
+    image_uploadtab: true,
+    images_upload_handler: cmsUpload
+  }
+
+  constructor() { }
+
+  ngOnInit(): void { 
+    MarketplaceSdkInstance.Configuration.Set({baseApiUrl: 'https://marketplace-middleware-test.azurewebsites.net'})
+    MarketplaceSDK.Tokens.SetAccessToken(this.ocToken);
+  }
 }
+
