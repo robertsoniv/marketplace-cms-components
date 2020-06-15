eb06e7e8df4411b2c90918386d5047c81c3102e1
Robert Watt <rwatt@four51.com>
2020-06-15 14:26:29 -0500

    :construction: page editor component
    properties:
    - *siteUrl* base url for pages)
    - *pageContentDoc* (optional) existing content doc for editing
    - *editorOptions* (optional) options to be passed to the internal cms-html-editor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
 import { AppComponent } from './app.component';
 import { HtmlEditorComponent } from './components/html-editor/html-editor.component';
+import { FormsModule } from '@angular/forms';
+import { PageEditorComponent } from './components/page-editor/page-editor.component';
 
 @NgModule({
-  declarations: [AppComponent, HtmlEditorComponent],
-  imports: [BrowserModule, NgbModule, EditorModule],
+  declarations: [AppComponent, HtmlEditorComponent, PageEditorComponent],
+  imports: [BrowserModule, FormsModule, NgbModule, EditorModule],
   providers: [
     { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' }
   ],
