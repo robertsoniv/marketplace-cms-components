8429edd2ae77e8c3244756e549b7dca7000232a0
Crhistian Ramirez <cramirez@four51.com>
2020-07-07 15:49:38 -0500

    get rid of commands, no longer necessary

--- plugin/src/commands/index.ts
+++
@@ -1,7 +0,0 @@
-import AssetPicker from './AssetPicker/AssetPicker';
-import AssetUploader from './AssetUploader/AssetUploader';
-
-export default editor => {
-  editor.addCommand('ocAssetPicker', AssetPicker);
-  editor.addCommand('ocAssetUploader', AssetUploader);
-};
